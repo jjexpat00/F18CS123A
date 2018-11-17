38c27ba5ac9b0e67e41ed7b2d3a7f40c1425a1c3
Eugenie Zhu <eugeniezhu@gmail.com>
2018-11-17 15:56:03 -0800

    -add test2 in blanktester and blank

--- src/Blank.java
+++ src/Blank.java
@@ -1,7 +1,13 @@
 public class Blank {
     private static String hello = "Hello World";
+    private static String goodbye = "Goodbye to a world";
 
     public static String test() {
         return hello;
     }
+
+    public static String test2() {
+        return goodbye;
+    }
+
 }
--- src/BlankTester.java
+++ src/BlankTester.java
@@ -1,5 +1,6 @@
 public class BlankTester {
     public static void main(String[] args) {
         System.out.println(Blank.test());
+        System.out.println(Blank.test2());
     }
 }
